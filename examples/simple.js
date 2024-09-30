73556d85a822755dd32c570594d8e5297710d3dc
heroku <dairys.queen@gmail.com>
2024-10-01 05:48:00 +0700

    Update simple.js

--- examples/simple.js
+++ examples/simple.js
@@ -1,18 +1,18 @@
 const twvoucher = require('@fortune-inc/tw-voucher');
 
 //ใช้โค๊ดอังเปา
-twvoucher('06x-xxx-xxxx', 'xxxxhFog10Ijbmg1c').then(redeemed => {
+twvoucher('082-989-5959', 'xxxxhFog10Ijbmg1c').then(redeemed => {
     // เติมลิ้งอังเปาแล้ว
-    console.log(`redeem ซอง ${redeemed.code} ของ ${redeemed.owner_full_name} จำนวน ${redeemed.amount} บาทแล้ว`) //redeem ซอง xxxxhFog10Ijbmg1c ของ สมชาย(ชื่อคนสร้างซอง) จำนวน 10 บาทแล้ว
+    console.log(`redeem ซอง ${redeemed.code} ของ ${redeemed.owner_full_name} จำนวน ${redeemed.amount} บาทแล้ว`) //redeem ซอง 0192450a886977712399a5339629a8081az ของ ชวัลรัสย์ จำนวน 10000 บาทแล้ว
 }).catch(err => {
     // ลิ้งอังเปาไม่ถูก / ใช้ไปแล้ว!
     console.error('invaild voucher code')
 })
 
 //ใช้ลิ้งอังเปา
-twvoucher('06x-xxx-xxxx', 'https://gift.truemoney.com/campaign/?v=xxxxfhFog10Ijbmg1c').then(redeemed => {
+twvoucher('082-989-5959', 'https://gift.truemoney.com/campaign/?v=0192450a886977712399a5339629a8081az').then(redeemed => {
     // เติมลิ้งอังเปาแล้ว
-    console.log(`redeem ซอง ${redeemed.code} ของ ${redeemed.owner_full_name} จำนวน ${redeemed.amount} บาทแล้ว`) //redeem ซอง xxxxhFog10Ijbmg1c ของ สมชาย(ชื่อคนสร้างซอง) จำนวน 10 บาทแล้ว
+    console.log(`redeem ซอง ${redeemed.code} ของ ${redeemed.owner_full_name} จำนวน ${redeemed.amount} บาทแล้ว`) //redeem ซอง 0192450a886977712399a5339629a8081az ของ ชวัลรัสย์ จำนวน 10000 บาทแล้ว
 }).catch(err => {
     // ลิ้งอังเปาไม่ถูก / ใช้ไปแล้ว!
     console.error('invaild voucher code')
