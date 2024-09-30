e0107fb7aa24e64b7d2b83ff2e39bbdca55062fb
heroku <dairys.queen@gmail.com>
2024-10-01 05:50:51 +0700

    Update express.js

--- examples/express.js
+++ examples/express.js
@@ -2,10 +2,10 @@ const twvoucher = require('@fortune-inc/tw-voucher');
 
 app.post("/api/v1/topup",async (req,res)=>{
     //ใส่ req.body ตรงได้ไม่ต้องกลัว object exploit
-    twvoucher('เบอร์โทรศัพท์',req.body).then(redeemed => {
+    twvoucher('082-989-5959',req.body).then(redeemed => {
         return res.json({
             success: true,
-            amount: redeemed.amount
+            amount: redeemed.10000
         })
     }).catch(() => {
         return res.json({
